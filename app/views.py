50efcd526c723ea86de2da9334409ef294550aa0
lisfer <makrushinds@gmail.com>
2014-10-18 18:47:05 +0300

    functions for adding, listing and editing united in BaseEntity class

--- app/views.py
+++ app/views.py
@@ -18,96 +18,106 @@ def replace_other_chars(string):
     return string
 
 
-def department_uniq_name(name):
-    """Creates uniq name_en for department:
+def entity_uniq_name(name, model):
+    """Creates uniq name_en for entity (model of entity should have fields 
+    name and name_en):
     transliterates name and if such name_en exists in database
     adds underline and first free number
     """
     name_en = replace_other_chars(name)
-    cnt = Department.query.filter(Department.name_en==name_en).count()
+    cnt = model.query.filter(model.name_en==name_en).count()
     if cnt > 0:
         i = 0
         while cnt > 0:
             i += 1
-            cnt = Department.query.filter(Department.name_en=='{}_{}'.format(name_en, i)).count()
+            cnt = model.query.filter(model.name_en=='{}_{}'.format(name_en, i)).count()
         rzlt = '{}_{}'.format(name_en, i)
     else:
         rzlt = name_en
     return rzlt
 
-def prepare_base_object(base_obj):
-    base_url = url_for(base_obj['func_list'])
-    base_url_new = url_for(base_obj['func_new'])
-    base_obj['base_url'] = base_url
-    base_obj['base_url_new'] = base_url_new
-    if 'url_parameter' in base_obj:
-        base_obj['base_url_edit'] = url_for(base_obj['func_edit'], 
-                url_parameter=base_obj['url_parameter'])
-        
+      
 
-def base_list(base_obj):
-    prepare_base_object(base_obj)
-    cnt = base_obj['model'].query.count()
-    if cnt == 0:
-        return redirect(base_obj['base_url_new'])
-    base_data = base_obj['model'].query.all()
-    return render_template(base_obj.get('template_list', 'base_list.html'),
-            base_data=base_data,
-            page_name=base_obj['name'],
-            add_item_url=base_obj['base_url_new'])
+class BaseEntity():
+    """ Entity model should have fields name and name_en. Second one is used for generating url"""
 
-def base_edit(base_obj):
-    prepare_base_object(base_obj)
-    base_form = base_obj['base_form']()
-    if base_form.validate_on_submit():
-        base_data_obj = base_obj['model'].query.filter(
-                base_obj['model'].name_en==base_obj['url_parameter']).first()
-        if base_data_obj is None:
-            pass
-        for a,b in base_form.data.items():
-            attr = getattr(base_data_obj, a, None)
-            if attr:
-                setattr(base_data_obj, a, b)
-        if db.session.is_modified(base_data_obj):
-            db.session.commit()    
+    def __init__(self, entity_name, url_param=None, template_list=None, template_edit=None, entity_form=None):
+        self.name = entity_name.lower()
+        self.model = globals()[entity_name.capitalize()]
+        self.form = entity_form if entity_form is not None else self.name.capitalize() + 'Form' 
+        print entity_form, self.form, self.name, entity_name
+        self.form = globals()[self.form]
+        self.entity_url = url_for(self.name + 's')
+        self.entity_url_new = url_for(self.name + '_new')
+        self.entity_url_edit = self.name + '_edit'
+        self.template_list = template_list if template_list is not None else 'base_list.html'
+        self.template_edit = template_edit if template_edit is not None else 'base_edit.html'
+        self.url_param = url_param
+        if self.url_param:
+            self.entity_url_edit = url_for(self.entity_url_edit, url_parameter = self.url_param)
 
-        if request.values.get('submited') == 'Save & continue editing':
-            return redirect(base_obj['base_url_edit'])
+     
+    def base_list(self):
+        """generates list of entites. If the list is empty - returns new form
+        to input new data"""
+        cnt = self.model.query.count()
+        if cnt == 0:
+            return redirect(self.entity_url_new)
+        base_data = self.model.query.all()
+        return render_template(self.template_list,
+                base_data=base_data,
+                page_name=self.name,
+                add_item_url=self.entity_url_new)
+
+
+    def base_edit(self):
+        form = self.form()
+        if form.validate_on_submit():
+            base_data = self.model.query.filter(
+                    self.model.name_en==self.url_param).first()
+            if base_data is None:
+                pass #!!!
+            for a,b in form.data.items():
+                setattr(base_data, a, b)
+            if db.session.is_modified(base_data):
+                db.session.commit()    
+
+            if request.values.get('submited') == 'Save & continue editing':
+                return redirect(self.entity_url_edit)
+            else:
+                return redirect(self.entity_url)
+
+        base_data = self.model.query.filter(
+            self.model.name_en==self.url_param).first()
+        if base_data is None:
+            pass #!!!
+        form = self.form(obj=base_data)
+        return render_template(self.template_edit,
+                page_name=self.name,
+                add_item_url=self.entity_url_new,
+                data=form)
+
+
+    def base_new(self):
+        form = self.form()
+        if form.validate_on_submit():
+            base_data = self.model()
+            for a,b in form.data.items():
+                setattr(base_data, a, b)
+            base_data.name_en = entity_uniq_name(base_data.name, self.model)
+            db.session.add(base_data)
+            db.session.commit()
+            if request.values.get('submited') == 'Save & continue editing':
+                return redirect(url_for(self.entity_url_edit, url_parameter=base_data.name_en))
+            else:
+                return redirect(self.entity_url)
+
         else:
-            return redirect(base_obj['base_url'])
-
-    base_data_obj = base_obj['model'].query.filter(
-        base_obj['model'].name_en==base_obj['url_parameter']).first()
-    if base_data_obj is None:
-        pass
-    base_form = base_obj['base_form'](obj=base_data_obj)
-    return render_template(base_obj.get('template_edit', 'base_edit.html'),
-            page_name=base_obj['name'],
-            add_item_url=base_obj['base_url_new'],
-            data=base_form)
-
-def base_new(base_obj):
-    prepare_base_object(base_obj)
-    base_form = base_obj['base_form']()
-    if base_form.validate_on_submit():
-        base_data_obj = base_obj['model']()
-        for a,b in base_form.data.items():
-            setattr(base_data_obj, a, b)
-        base_data_obj.name_en = replace_other_chars(base_data_obj.name)
-        db.session.add(base_data_obj)
-        db.session.commit()
-        if request.values.get('submited') == 'Save & continue editing':
-            print base_obj['func_edit'], base_data_obj.name, base_data_obj.name_en
-            return redirect(url_for(base_obj['func_edit'], 
-                url_parameter=base_data_obj.name_en))
-        else:
-            return redirect(base_obj['base_url'])
-    else:
-        base_form = base_obj['base_form']()
-        return render_template(base_obj.get('template_edit', 'base_edit.html'),
-            page_name=base_obj['name'],
-            add_item_url=base_obj['base_url_new'],
-            data=base_form)
+            form = self.form()
+            return render_template(self.template_edit,
+                page_name=self.name,
+                add_item_url=self.entity_url_new,
+                data=form)
 
 
 
@@ -119,34 +129,18 @@ def index():
 
 @app.route('/departments/')
 def departments():
-    dep_obj = {'model' : Department,
-                'func_list' : 'departments', #function name
-                'func_new' : 'department_new', #function name
-                'name': 'Department'}
-    return base_list(dep_obj)
+    depart = BaseEntity('department')
+    return depart.base_list()
 
 
 @app.route('/departments/<url_parameter>/', methods=['GET', 'POST'])
-def department(url_parameter):
-    dep_obj = {'model' : Department,
-                'base_form': DepartmentForm,
-                'url_parameter': url_parameter,
-                'func_list' : 'departments', #function name
-                'func_new' : 'department_new', #function name
-                'func_edit' : 'department',
-                'name': 'Department'}
-    
-    return base_edit(dep_obj)
+def department_edit(url_parameter):
+    depart = BaseEntity('department', url_param=url_parameter)
+    return depart.base_edit()
 
 
 @app.route('/departments/new/', methods=['GET', 'POST'])
 def department_new():
-    dep_obj = {'model' : Department,
-                'base_form': DepartmentForm,
-                'func_list' : 'departments', #function name
-                'func_new' : 'department_new', #function name
-                'func_edit' : 'department',
-                'name': 'Department'}
-
-    return base_new(dep_obj)
+    depart = BaseEntity('department')
+    return depart.base_new()
     
