c410eb3c316d616489437790143fc3144874d055
lisfer <makrushinds@gmail.com>
2014-10-19 14:28:19 +0300

    error with creating database (forms for models need models to exist). Now that code replaced to init methods

--- app/forms.py
+++ app/forms.py
@@ -4,14 +4,18 @@ from wtforms.validators import DataRequired, NoneOf
 from app.models import Department, User, Hardware
 
 class UserForm(Form):
-	login = StringField('login', validators=[DataRequired(), 
-            NoneOf([''], 'cannot be empty', None)])
-	name = StringField('name', validators=[DataRequired(), 
-            NoneOf([''], 'cannot be empty', None)])
-	surname = StringField('surname', validators=[DataRequired(), 
-            NoneOf([''], 'cannot be empty', None)])
-        department_id = SelectField('department', coerce=int,  
-            choices=[(i.id, i.name) for i in Department.query.all()])
+    login = StringField('login', validators=[DataRequired(), 
+        NoneOf([''], 'cannot be empty', None)])
+    name = StringField('name', validators=[DataRequired(), 
+        NoneOf([''], 'cannot be empty', None)])
+    surname = StringField('surname', validators=[DataRequired(), 
+        NoneOf([''], 'cannot be empty', None)])
+    department_id = SelectField('department', coerce=int,  
+        choices=[])
+
+    def __init__(self, *args, **kwargs):
+        super(UserForm, self).__init__(*args, **kwargs)
+        self.department_id.choices=[(i.id, i.name) for i in Department.query.all()]
 
 
 class DepartmentForm(Form):
@@ -19,30 +23,38 @@ class DepartmentForm(Form):
             NoneOf([''], 'cannot be empty', None)])
 
 class HardwareForm(Form):
-	serial = StringField('serial', validators=[DataRequired(), 
-            NoneOf([''], 'cannot be empty', None)])
-	inventory = StringField('inventory', validators=[DataRequired(), 
-            NoneOf([''], 'cannot be empty', None)])
-	model = StringField('model', validators=[DataRequired(), 
-            NoneOf([''], 'cannot be empty', None)])
-	name = StringField('name', validators=[DataRequired(), 
-            NoneOf([''], 'cannot be empty', None)])
-        department_id = SelectField('department', coerce=int,  
-            choices=[(i.id, i.name) for i in Department.query.all()])
-        user_id = SelectField('user', choices=[(i.id, i.name) for i in User.query.all()], coerce=int)
-	state = SelectField('state', 
-                coerce=int,
-		choices=[(1, 'In use'),
-                            (2, 'Under repair'), 
-                            (3, 'Free'),
-		])
+    serial = StringField('serial', validators=[DataRequired(), 
+        NoneOf([''], 'cannot be empty', None)])
+    inventory = StringField('inventory', validators=[DataRequired(), 
+        NoneOf([''], 'cannot be empty', None)])
+    model = StringField('model', validators=[DataRequired(), 
+        NoneOf([''], 'cannot be empty', None)])
+    name = StringField('name', validators=[DataRequired(), 
+        NoneOf([''], 'cannot be empty', None)])
+    department_id = SelectField('department', coerce=int,  
+        choices=[])
+    user_id = SelectField('user', choices=[], coerce=int)
+    state = SelectField('state', 
+            coerce=int,
+            choices=[(1, 'In use'),
+                        (2, 'Under repair'), 
+                        (3, 'Free'),
+            ])
+  
+    def __init__(self, *args, **kwargs):
+        super(UserForm, self).__init__(*args, **kwargs)
+        self.department_id.choices = [(i.id, i.name) for i in Department.query.all()]
+        self.user_id.choices = [(i.id, i.name) for i in User.query.all()]
 
 class SoftwareForm(Form):
-	name = StringField('name', validators=[DataRequired(), 
-        NoneOf([''], 'cannot be empty', None)])
-	comp_id = SelectField('comp', choices=[(i.id, i.name) for i in Hardware.query.all()],coerce=int)
-	state = SelectField('state', 
-                coerce=int,
-		choices=[(1, 'In use'),
-                            (2, 'Free'),
-		])
+    name = StringField('name', validators=[DataRequired(), 
+    NoneOf([''], 'cannot be empty', None)])
+    comp_id = SelectField('comp', choices=[],coerce=int)
+    state = SelectField('state', 
+            coerce=int,
+            choices=[(1, 'In use'),
+                        (2, 'Free'),
+            ])
+    def __init__(self, *args, **kwargs):
+        super(UserForm, self).__init__(*args, **kwargs)
+        self.user_id.choices = [(i.id, i.name) for i in Hardware.query.all()]
--- app/views.py
+++ app/views.py
@@ -2,8 +2,8 @@
 
 from flask import render_template, flash, redirect, session, url_for, request, g
 from app import app, db
-from forms import UserForm, DepartmentForm, HardwareForm, SoftwareForm
 from models import User, Department, Hardware, Software
+from forms import UserForm, DepartmentForm, HardwareForm, SoftwareForm
 import re
 import types
 
