0e2f297586b5b56897148e3b032ef6fb93d05e3b
Anton Makrushyn <ansermak@gmail.com>
2015-01-19 13:22:31 +0200

    Add session timeout

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+import os
+from datetime import timedelta
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'venividivici'
+app.config['SECRET_KEY'] = os.urandom(24)
+app.permanent_session_lifetime = timedelta(seconds=600)
 app.config.from_object('config')
 db = SQLAlchemy(app)
 
