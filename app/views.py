169b4e40e4d80c7449327cb6158b82056c8a788e
Anton Makrushyn <ansermak@gmail.com>
2015-04-17 18:25:20 +0300

    Change appearance

--- app/views.py
+++ app/views.py
@@ -165,7 +165,7 @@ class BaseEntity(object):
     def _save_validated_form(self, form):
         base_data = self._get_base_data()
         self._save_data(base_data, form)
-        if request.values.get('submitted') == 'Save & new':
+        if request.values.get('submitted') == 'Save & New':
             return self.entity_url_new
         elif request.values.get('submitted') == 'Delete':
             self._delete_data(base_data)
